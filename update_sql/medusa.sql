f9b84170bdf3eee688f97548a82ecfc4aa88f589
honganh <honganhnguye08102003@gmail.com>
2025-05-04 18:10:54 +0700

    test mod

--- update_sql/medusa.sql
+++ update_sql/medusa.sql
@@ -1,23 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.6.23 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping database structure for medusa
-CREATE DATABASE IF NOT EXISTS `medusa` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `medusa`;
 
 -- Dumping structure for table medusa.account
 CREATE TABLE IF NOT EXISTS `account` (
