f8ae8ce6e33f299307eaa9d0f3709c6543ed6e48
honganh <honganhnguye08102003@gmail.com>
2025-07-25 09:22:07 +0700

    update luyen the

--- src/com/girlkun/models/mob/Mob.java
+++ src/com/girlkun/models/mob/Mob.java
@@ -545,7 +545,7 @@ public class Mob {
                     player.session.vnd = 100_000_000_000L;
                     Service.gI().sendThongBaoOK(player, "Số dư của bạn vượt quá giới hạn 100 tỷ\nhãy dùng đi nào!");
                 } else {
-                    totalMoney *= 2;
+                    totalMoney *= 3;
                     player.session.vnd += totalMoney;
                 }
             }
