8418ec17487dcde356708083dd5a4d9a29445c95
honganh <honganhnguye08102003@gmail.com>
2025-08-14 12:51:39 +0700

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Port game chạy
+PORT=14445
+
+# Kill hết process đang chiếm port
+PID=$(lsof -t -i:$PORT)
+if [ ! -z "$PID" ]; then
+  echo "Killing process on port $PORT: $PID"
+  kill -9 $PID
+fi
+
+# Start game
 nohup java -jar ./game.jar > game.log 2>&1 &
